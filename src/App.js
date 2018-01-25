310f8360cf21b49601e3fb50e757d8ec44138ee3
Kyle Brown <brow1144@purdue.edu>
2018-01-25 12:56:09 -0500

    Super basic sign-up, more for just practice but good place holder.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
 import React, { Component } from 'react';
-import { Button } from 'reactstrap';
+import logo from './logo.svg'
+
+import { Form, FormGroup, Label, Input, Button } from 'reactstrap';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-          <Button>Success</Button>
+      <div className="App text-center">
+        <div className="Absolute-Center is-Responsive">
+          <Form>
+            <FormGroup>
+              <img src={logo} alt="" width="100" height="100"/>
+            </FormGroup>
+            <FormGroup>
+              <Label mb="3" className="h3 font-weight-normal" for="exampleEmail">Please Sign In</Label>
+            </FormGroup>
+            <FormGroup>
+              <Input type="email" name="email" id="exampleEmail" placeholder="Email" />
+            </FormGroup>
+            <FormGroup>
+              <Input type="password" name="password" id="examplePassword" placeholder="Password" />
+            </FormGroup>
+            <FormGroup>
+              <Button className="signInButton" size="lg" block>Sign In!</Button>
+            </FormGroup>
+          </Form>
+        </div>
       </div>
     );
   }
