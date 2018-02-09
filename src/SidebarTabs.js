4177ef3136db11722bd2608734a45f55553488d1
Kyle Brown <brow1144@purdue.edu>
2018-02-09 10:30:27 -0500

    Make logo take user back to home page

--- src/SidebarTabs.js
+++ src/SidebarTabs.js
@@ -9,7 +9,9 @@ import './SidebarTabs.css'
 const Sidebar = () => {
   return (
     <div>
-      <img src={logo} alt="Logo" />
+      <NavLink style={{ textDecoration: 'none' }} to={`/HomePage`}>
+        <img src={logo} alt="Logo" />
+      </NavLink>
 
       <NavLink style={{ textDecoration: 'none' }} to={`/HomePage/cs307`}>
         <p className="classOne" >CS 307</p>
@@ -18,7 +20,6 @@ const Sidebar = () => {
       <NavLink style={{ textDecoration: 'none' }} to={`/HomePage/cs252`}>
         <p className="classOne" >CS 252</p>
       </NavLink>
-
 
     </div>
   )
