b7fc486c1d8271af03fa1f1a8c014952d6c87bea
Walter Jacquette <wjacquet@purdue.edu>
2018-02-09 14:24:07 -0500

    Settings SideBar Intergrated
    SideBar from main homepage was altered and integrated for the purposes of the settings page.

--- src/Settings.js
+++ src/Settings.js
@@ -1,15 +1,102 @@
 import React, { Component } from 'react';
+import { Button } from 'reactstrap';
+
+import Sidebar from 'react-sidebar';
+import SettingsSide from './SettingsSide';
 
 import './Settings.css'
 
+const mql = window.matchMedia(`(min-width: 800px)`);
+
+
 class Settings extends Component {
-  render() {
-    return (
-      <div>
-        <b> MY! Settings Page </b>
-      </div>
-    )
-  }
-};
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            mql: mql,
+            docked: props.docked,
+            open: props.open,
+            sideButtonVisibility: !props.docked,
+        };
+    }
+
+    dockSideBar = () => {
+        if (this.state.sidebarDocked)
+            this.setState({
+                sidebarOpen: false,
+                sideButtonVisibility: true,
+            });
+        else
+            this.setState({
+                sidebarOpen: true,
+                sideButtonVisibility: false,
+            });
+    };
+
+
+    onSetSidebarOpen = (open) => {
+        this.setState({
+            sidebarOpen: open,
+            sideButtonVisibility: true,
+        });
+    };
+
+    componentWillMount() {
+        mql.addListener(this.mediaQueryChanged);
+        this.setState({
+            mql: mql,
+            sidebarDocked: mql.matches,
+            sideButtonVisibility: !this.state.mql.matches,
+        });
+    };
+
+    componentWillUnmount() {
+        this.state.mql.removeListener(this.mediaQueryChanged);
+    };
+
+    mediaQueryChanged = () => {
+        this.setState({
+            sidebarDocked: this.state.mql.matches,
+            sideButtonVisibility: !this.state.mql.matches,
+        });
+    };
+
+    render() {
+
+        let sidebarContent = <SettingsSide />;
+
+        const sidebarStyles = {
+            sidebar: {
+                backgroundColor: 'f3f3f3',
+                width: '8em',
+                textAlign: 'center',
+            },
+            overlay: {
+                backgroundColor: '#f3f3f3'
+            },
+        };
+
+        return (
+            <Sidebar styles={sidebarStyles}
+                     sidebar={sidebarContent}
+                     open={this.state.sidebarOpen}
+                     docked={this.state.sidebarDocked}
+                     onSetOpen={this.onSetSidebarOpen}>
+
+                {this.state.sideButtonVisibility
+                    ?
+                    <Button outline onClick={this.dockSideBar}>
+                        <i className="fas fa-bars"/>
+                    </Button>
+                    :
+                    <b>Start of the Settings</b>
+
+                }
+
+            </Sidebar>
+        );
+    }
+}
 
 export default Settings
