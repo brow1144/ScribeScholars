619c4f3ac3931fa01a5bed27c532a39523c6a039
Kyle Brown <brow1144@purdue.edu>
2018-02-09 11:46:24 -0500

    Make settings page responsive on all devices

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 
-import Sidebar from 'react-sidebar';
+import { Button } from 'reactstrap';
 
+import Sidebar from 'react-sidebar';
 import SidebarTabs from './SidebarTabs';
 
 import './HomePage.css';
@@ -16,18 +17,39 @@ class HomePage extends Component {
     this.state = {
       mql: mql,
       docked: props.docked,
-      open: props.open
+      open: props.open,
+      sideButtonVisibility: !props.docked,
     };
-
   }
 
+  dockSideBar = () => {
+    if (this.state.sidebarDocked)
+      this.setState({
+        sidebarOpen: false,
+        sideButtonVisibility: true,
+      });
+    else
+      this.setState({
+        sidebarOpen: true,
+        sideButtonVisibility: false,
+      });
+  };
+
+
   onSetSidebarOpen = (open) => {
-    this.setState({sidebarOpen: open});
+    this.setState({
+      sidebarOpen: open,
+      sideButtonVisibility: true,
+    });
   };
 
   componentWillMount() {
     mql.addListener(this.mediaQueryChanged);
-    this.setState({mql: mql, sidebarDocked: mql.matches});
+    this.setState({
+      mql: mql,
+      sidebarDocked: mql.matches,
+      sideButtonVisibility: !this.state.mql.matches,
+    });
   };
 
   componentWillUnmount() {
@@ -35,25 +57,45 @@ class HomePage extends Component {
   };
 
   mediaQueryChanged = () => {
-    this.setState({sidebarDocked: this.state.mql.matches});
+    this.setState({
+      sidebarDocked: this.state.mql.matches,
+      sideButtonVisibility: !this.state.mql.matches,
+    });
   };
 
   render() {
 
     let sidebarContent = <SidebarTabs />;
 
-    // var sidebarProps = {
-    //   sidebar: this.state.sidebarOpen,
-    //   docked: this.state.sidebarDocked,
-    //   onSetOpen: this.onSetSidebarOpen
-    // };
-
     const sidebarStyles = {
       sidebar: {
         backgroundColor: 'f3f3f3',
         width: '8em',
         textAlign: 'center',
-      }
+      },
+      content: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        overflowY: 'scroll',
+        WebkitOverflowScrolling: 'touch',
+        transition: 'left .3s ease-out, right .3s ease-out',
+      },
+      overlay: {
+        zIndex: 1,
+        position: 'fixed',
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        opacity: 0,
+        visibility: 'hidden',
+        transition: 'opacity .3s ease-out, visibility .3s ease-out',
+        //backgroundColor: 'rgba(0,0,0,.3)',
+        backgroundColor: '#f3f3f3'
+      },
     };
 
     return (
@@ -62,7 +104,16 @@ class HomePage extends Component {
                open={this.state.sidebarOpen}
                docked={this.state.sidebarDocked}
                onSetOpen={this.onSetSidebarOpen}>
-        <p>Main Section</p>
+
+        {this.state.sideButtonVisibility
+          ?
+          <Button outline onClick={this.dockSideBar}>
+            <i className="fas fa-bars"/>
+          </Button>
+          :
+          <b>Main Section</b>
+        }
+
       </Sidebar>
     );
   }
