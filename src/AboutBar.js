b6eb708ef80fcfa0a17854ce989e3480752c4f28
Jeremy <jeremyputput@gmail.com>
2018-02-11 14:16:02 -0500

    Nav bar overhaul
    Its made with tabs now

--- src/AboutBar.js
+++ src/AboutBar.js
@@ -1,29 +1,82 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom'
+import React, { Component } from 'react';
+import { TabContent, TabPane, Nav, NavItem, NavLink, Card, Button, CardTitle, CardText, Row, Col } from 'reactstrap';
 
+import classnames from 'classnames';
 import './AboutBar.css'
 
 class AboutBar extends React.Component {
 
+    constructor(props) {
+        super(props);
+
+        this.toggle = this.toggle.bind(this);
+        this.state = {
+            activeTab: '1'
+        };
+    }
+
+    toggle(tab) {
+        if (this.state.activeTab !== tab) {
+            this.setState({
+                activeTab: tab
+            });
+        }
+    }
     render() {
         return (
-            <div className={"grid"}>
-                <NavLink style={{ textDecoration: 'none' }} to={`/About/Student`}>
-                    <p className="button" >Students</p>
-                </NavLink>
-
-                <NavLink style={{ textDecoration: 'none' }} to={`/About/Teacher`}>
-                    <p className="button" >Teachers</p>
-                </NavLink>
-                <NavLink style={{ textDecoration: 'none' }} to={`/About/Administrator`}>
-                    <p className="button" >Administrators</p>
-                </NavLink>
-                <NavLink style={{ textDecoration: 'none' }} to={`/About/Parent`}>
-                    <p className="button" >Parents</p>
-                </NavLink>
+            <div>
+                <Nav tabs>
+                    <NavItem>
+                        <NavLink className={classnames({ active: this.state.activeTab === '1' })} onClick={() => { this.toggle('1'); }}>
+                            <p>Students</p>
+                        </NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink className={classnames({ active: this.state.activeTab === '2' })} onClick={() => { this.toggle('2'); }}><p>Teachers</p></NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink className={classnames({ active: this.state.activeTab === '3' })} onClick={() => { this.toggle('3'); }}><p>Administrators</p></NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink className={classnames({ active: this.state.activeTab === '4' })} onClick={() => { this.toggle('4'); }}><p>Parents</p></NavLink>
+                    </NavItem>
+                </Nav>
+                <TabContent activeTab={this.state.activeTab}>
+                    <TabPane tabId="1">
+                        <Row>
+                            <Col className={"col"}>
+                                <h4>Tab 1 Contents</h4>
+                            </Col>
+                        </Row>
+                    </TabPane>
+                    <TabPane tabId="2">
+                        <Row>
+                            <Col className={"col"}>
+                                <Card body>
+                                    <CardTitle>Special Title Treatment</CardTitle>
+                                    <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
+                                    <Button>Go somewhere</Button>
+                                </Card>
+                            </Col>
+                            <Col className={"col"}>
+                                <Card body>
+                                    <CardTitle>Special Title Treatment</CardTitle>
+                                    <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
+                                    <Button>Go somewhere</Button>
+                                </Card>
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col className={"col"}>
+                                <h1>Look at me</h1>
+                            </Col>
+                        </Row>
+                    </TabPane>
+                </TabContent>
             </div>
         );
     }
 }
+
 
 export default AboutBar
--- src/AboutPage.js
+++ src/AboutPage.js
@@ -10,15 +10,8 @@ class AboutPage extends Component {
         return (
             <div>
                 <div className={"container"}>
-                    <div className="nav"> /*This is the top of the page*/
+                    <div className="nav">
                         <AboutBar/>
-                    </div>
-                    <div className={"picture"}>
-                        <img src={img}/>
-                    </div>
-                    <div className={"rect"}>Text here</div>
-                    <div className={"picture"}>
-                        <img src={img}/>
                     </div>
                 </div>
             </div>
