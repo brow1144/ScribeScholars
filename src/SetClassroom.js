16ff5af6fc498bbf7c06549e3f57452ff1a458aa
Walter Jacquette <wjacquet@purdue.edu>
2018-02-11 19:34:21 -0500

    Classroom selection table created in settings

--- src/SetClassroom.js
+++ src/SetClassroom.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import { Button, Container, Row, Col, Form, FormGroup, Label, Input, FormText} from 'reactstrap';
+import { Button, Container, Row, Col, Form, FormGroup, Label, Input, InputGroup, InputGroupAddon, Modal, ModalBody, ModalFooter, ModalHeader} from 'reactstrap';
 
 import './SetClassroom.css';
 
@@ -20,7 +20,22 @@ class SetClassroom extends Component
                 <Row className={"Filler"}> </Row>
                 <Row className={"BoxForm"}>
                     <Col xs={"12"}>
+                        <Form>
+                            <FormGroup row>
+                                <Label size={"lg"} for="exampleSelectMulti" sm={2}>Enrolled Courses:</Label>
+                                <Col sm={5}>
+                                    <Input className="ClassSelection" size="lg" type="select" name="selectMulti" id="exampleSelectMulti" multiple>
+                                        <option>AP CompSci</option>
+                                        <option>Chemistry</option>
+                                        <option>AP Calculus</option>
+                                        <option>Literature</option>
+                                        <option>Band</option>
+                                    </Input>
+                                </Col>
+                            </FormGroup>
 
+
+                        </Form>
                     </Col>
                 </Row>
             </Container>
