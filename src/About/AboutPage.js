1ca02c3c8f9092f83a8c347c4587f06634c4aef8
Jeremy <jeremyputput@gmail.com>
2018-02-18 16:12:03 -0500

    Added Administrator section

--- src/About/AboutPage.js
+++ src/About/AboutPage.js
@@ -22,14 +22,14 @@ class AboutPage extends Component {
                             <h1>Scribe Scholars' Mission</h1>
                             <p>We are dedicated to making a better in-class experience for teachers and students.</p>
                             <p>Interactive assignments, grades, and in website chat board are just some of the features
-                                that make scribe scholars the premier learning experience</p>
+                                that make Scribe Scholars the premier learning experience</p>
                         </Col>
                     </Row>
                     <Row className="row stu">
                         <a name="headline1"></a>
                         <Col className="col student stu">
                         </Col>
-                        <Col className="col text stu" /*-12 col-lg-6 offset-lg-1 text-left"*/>
+                        <Col lg="6" className="col text stu" >
                             <h1 className="">Students</h1>
                             <p>Students will have full access to their grades at a momments notice.  In-class activities can be set up by teachers
                                 to make the classroom engaging and fun.  Adding classes is easy to do and all located within our website.  The message
@@ -40,9 +40,20 @@ class AboutPage extends Component {
                     <Row className="row teach">
                         <Col className="col text teach">
                             <h1>Teachers</h1>
-                            <p>There are a wide variety of features available to teachers for use in the class room</p>
+                            <p>There are a wide variety of features available to teachers for use in the class room.  In-class activities keep students
+                                engaged and interested in learning.  Grades are simple to update to allow students to track their progress.  Teachers can
+                                monitor class progress and identify where the class can most improve.</p>
                         </Col>
                         <Col className="col teacher">
+                        </Col>
+                    </Row>
+                    <Row className="row admin">
+                        <Col className="col administrator">
+                        </Col>
+                        <Col className="col text admin">
+                            <h1>Administrators</h1>
+                            <p>Managing has never been simplier.  Administrators can easily manage classrooms and teachers with our straight-forward
+                                process for setting up classes.  </p>
                         </Col>
                     </Row>
                 </div>
