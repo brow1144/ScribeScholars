fc9209cc02bec877c12ca3fe37bce7d0125ae29a
Kyle Brown <brow1144@purdue.edu>
2018-02-19 18:51:59 -0500

    Fix compile error

--- src/HomePage/Side.js
+++ src/HomePage/Side.js
@@ -19,30 +19,29 @@ class Side extends Component {
     // Add Firebase Code to get image.
   }
 
-    render() {
-      return (
-        <div>
-          <NavLink style={{textDecoration: 'none'}} to={`/HomePage`}>
-            <img className="logo" src={logo} alt="Logo"/>
+  render() {
+    return (
+      <div>
+        <NavLink style={{textDecoration: 'none'}} to={`/HomePage`}>
+          <img className="logo" src={logo} alt="Logo"/>
+        </NavLink>
+
+        {Object.keys(this.props.classes).map((key, index) => {
+          return <NavLink key={key} style={{textDecoration: 'none'}}
+                          to={`/HomePage/${this.props.classes[index].class}`}>
+            <p className="classSide">{this.props.classes[index].class}</p>
           </NavLink>
+        })}
 
-          {Object.keys(this.props.classes).map((key, index) => {
-            return <NavLink key={key} style={{textDecoration: 'none'}}
-                            to={`/HomePage/${this.props.classes[index].class}`}>
-              <p className="classSide">{this.props.classes[index].class}</p>
-            </NavLink>
-          })}
-
-          <NavLink style={{textDecoration: 'none'}} to={`/settings`}>
-            <img className="settingsLogo"
-                 src={"https://firebasestorage.googleapis.com/v0/b/scribescholars-ad86f.appspot.com/o/userImage.jpg?alt=media&token=c3319c17-22b0-46de-9fcc-ca7bde927d9d"}
-                 alt="userIcon"/>
+        <NavLink style={{textDecoration: 'none'}} to={`/settings`}>
+          <img className="settingsLogo"
+               src={"https://firebasestorage.googleapis.com/v0/b/scribescholars-ad86f.appspot.com/o/userImage.jpg?alt=media&token=c3319c17-22b0-46de-9fcc-ca7bde927d9d"}
+               alt="userIcon"/>
           </NavLink>
-
         </div>
       )
     }
-  };
-}
+  }
+
 
 export default Side
