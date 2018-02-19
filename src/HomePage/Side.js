e12f617ecf95a33d78b259509686273e1ba87fac
Kyle Brown <brow1144@purdue.edu>
2018-02-19 18:28:00 -0500

    Add minor updates to home page

--- src/HomePage/Side.js
+++ src/HomePage/Side.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 
 import { NavLink } from 'react-router-dom'
 
@@ -6,25 +6,34 @@ import logo from '../logo.svg';
 
 import './Side.css'
 
-const Sidebar = (props) => {
-  return (
-    <div>
-      <NavLink style={{ textDecoration: 'none' }} to={`/HomePage`}>
-        <img className="logo" src={logo} alt="Logo" />
-      </NavLink>
+class Sidebar extends Component {
+  constructor(props) {
+    super(props);
 
-      {Object.keys(props.classes).map((key, index) => {
-        return <NavLink key={key} style={{ textDecoration: 'none' }} to={`/HomePage/${props.classes[index].class}`}>
-                  <p className="classSide"  >{props.classes[index].class}</p>
-               </NavLink>
-      })}
+  }
 
-      <NavLink style={{ textDecoration: 'none' }} to={`/settings`}>
-        <img className="settingsLogo" src={"https://firebasestorage.googleapis.com/v0/b/scribescholars-ad86f.appspot.com/o/userImage.jpg?alt=media&token=c3319c17-22b0-46de-9fcc-ca7bde927d9d"} alt="userIcon" />
-      </NavLink>
+  render() {
+    return (
+      <div>
+        <NavLink style={{textDecoration: 'none'}} to={`/HomePage`}>
+          <img className="logo" src={logo} alt="Logo"/>
+        </NavLink>
 
-    </div>
-  )
+        {Object.keys(this.props.classes).map((key, index) => {
+          return <NavLink key={key} style={{textDecoration: 'none'}} to={`/HomePage/${this.props.classes[index].class}`}>
+            <p className="classSide">{this.props.classes[index].class}</p>
+          </NavLink>
+        })}
+
+        <NavLink style={{textDecoration: 'none'}} to={`/settings`}>
+          <img className="settingsLogo"
+               src={"https://firebasestorage.googleapis.com/v0/b/scribescholars-ad86f.appspot.com/o/userImage.jpg?alt=media&token=c3319c17-22b0-46de-9fcc-ca7bde927d9d"}
+               alt="userIcon"/>
+        </NavLink>
+
+      </div>
+    )
+  }
 };
 
 export default Sidebar
