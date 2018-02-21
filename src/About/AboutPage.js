9584cfd3f9405fbe49a547247205d4c2d4244dee
Jeremy <jeremyputput@gmail.com>
2018-02-20 23:48:16 -0500

    Adjusted title formatting

--- src/About/AboutPage.js
+++ src/About/AboutPage.js
@@ -31,13 +31,13 @@ class AboutPage extends Component {
 
                     <Row>
                         <Col className="about">
-                            <p>About Scribe Scholars</p>
+                            <p className={"about"}>About Scribe Scholars</p>
                         </Col>
                     </Row>
 
                     <Row className="bgOne">
                         <Col xs={12} className="text bgOne">
-                            <h1>Scribe Scholars' Mission</h1>
+                            <h1 className={"sectionTitle"}>Scribe Scholars' Mission</h1>
                             <p className="para">We are dedicated to making a better in-class experience for teachers and students.  Interactive assignments, grades, and in website chat board are just some of the features that make Scribe Scholars the premier learning experience</p>
                         </Col>
                     </Row>
@@ -46,7 +46,7 @@ class AboutPage extends Component {
                             <a name="headline1"><img src={student} className="picture" alt=""/></a>
                         </Col>
                         <Col xs={12} lg={6} className="text bgTwo">
-                            <h1 className="">Students</h1>
+                            <h1 className={"sectionTitle"}>Students</h1>
                             <p className="para">Students will have full access to their grades at a momments notice.  In-class activities can be set up by teachers
                                 to make the classroom engaging and fun.  Adding classes is easy to do and all located within our website.  The message
                                 board provides an environment for students to interact with each other and their teachers
@@ -56,7 +56,7 @@ class AboutPage extends Component {
                     <Row className="bgOne">
 
                         <Col xs={12} lg={6} className="text bgOne">
-                            <a name="headline2"><h1>Teachers</h1></a>
+                            <a name="headline2"><h1 className={"sectionTitle"}>Teachers</h1></a>
                             <p className="para">There are a wide variety of features available to teachers for use in the class room.  In-class activities keep students
                                 engaged and interested in learning.  Grades are simple to update to allow students to track their progress.  Teachers can
                                 monitor class progress and identify where the class can most improve.</p>
@@ -70,14 +70,14 @@ class AboutPage extends Component {
                             <a name="headline2"><img src={admin} className={"picture"} alt={""}/></a>
                         </Col>
                         <Col xs={12} lg={6} className="text bgTwo">
-                            <h1>Administrators</h1>
+                            <h1 className={"sectionTitle"}>Administrators</h1>
                             <p className="para">Managing has never been simplier.  Administrators can easily manage classrooms and teachers with our straight-forward
                                 process for setting up classes.  </p>
                         </Col>
                     </Row>
                     <Row className="bgOne">
                         <Col xs={12} lg={6} className="text bgOne">
-                            <a name="headline4"><h1>Parents</h1></a>
+                            <a name="headline4"><h1 className={"sectionTitle"}>Parents</h1></a>
                             <p className="para">Add parents stuff here</p>
                         </Col>
                         <Col xs={12} lg={6} className="bgOne">
@@ -86,7 +86,7 @@ class AboutPage extends Component {
                     </Row>
                     <Row className="bgTwo">
                         <Col xs={12} className={"contact bgTwo text"}>
-                            <a name="headline5"><h1>Contact Us</h1></a>
+                            <a name="headline5"><h1 className={"sectionTitle"}>Contact Us</h1></a>
                             <p className="para">Message the scribe scholars team at scribescholars@gmail.com</p>
                         </Col>
                     </Row>
