11bfc0895d6fa764d910221b4ff01eeca1f975cd
Kyle Brown <brow1144@purdue.edu>
2018-03-17 14:51:52 -0400

    Fix styling for announcements

--- src/HomePage/Cards.js
+++ src/HomePage/Cards.js
@@ -5,28 +5,23 @@ import {Card, CardHeader, CardTitle, CardText, CardDeck, CardSubtitle, CardBody,
 const Cards = (props) => {
 
   return (
-    <div>
-      <Row>
-        {/*<Col xs={{ size: '10', offset: 1 }}>*/}
-        {/*<Col md={{ size: '2', offset: 2 }}>*/}
-          <CardDeck>
-            {Object.keys(props.announcements).map((key, index) => {
-              return <Col key={key} md="4">
-                      <Card key={key}>
-                        <CardHeader>{props.announcements[index].class}</CardHeader>
-                        <CardBody>
-                            <CardTitle>{props.announcements[index].title}</CardTitle>
-                            <CardSubtitle>{props.announcements[index].subtitle}</CardSubtitle>
-                            <CardText>{props.announcements[index].message}</CardText>
-                          </CardBody>
-                     </Card>
-                     <br />
-                    </Col>
-            })}
-          </CardDeck>
-      </Row>
-
-    </div>
+      <div>
+        <Row>
+        {Object.keys(props.announcements).map((key, index) => {
+          return <Col sm={4} key={key}>
+            <Card key={key}>
+              <CardHeader>{props.announcements[index].class}</CardHeader>
+              <CardBody>
+                <CardTitle>{props.announcements[index].title}</CardTitle>
+                <CardSubtitle>{props.announcements[index].subtitle}</CardSubtitle>
+                <CardText>{props.announcements[index].message}</CardText>
+              </CardBody>
+            </Card>
+            <br />
+          </Col>
+        })}
+        </Row>
+      </div>
   )
 };
 
