cce759b84a301ecd5b51fbfc8ce5abd85ec64c52
Riley Robertson <rober343@purdue.edu>
2018-03-23 19:23:14 -0400

    Now calling homeworks and students/removed console.logs

--- src/Dashboard/AssignmentCards.js
+++ src/Dashboard/AssignmentCards.js
@@ -1,26 +1,27 @@
 import React from 'react'
 
-import {Card, CardHeader, CardTitle, CardText, CardSubtitle, CardBody, Row, Col } from 'reactstrap';
+import {NavLink, Card, CardTitle, Row, Col } from 'reactstrap';
 
 const AssignmentCards = (props) => {
 
     return (
         <div>
-            <Row>
-                {Object.keys(props.announcements).map((key, index) => {
+                {Object.keys(props.homeworks).map((key, index) => {
                     return <Col sm={4} key={key}>
-                        <Card key={key}>
-                            <CardHeader>{props.announcements[index].class}</CardHeader>
-                            <CardBody>
-                                <CardTitle>{props.announcements[index].title}</CardTitle>
-                                <CardSubtitle>{props.announcements[index].subtitle}</CardSubtitle>
-                                <CardText>{props.announcements[index].message}</CardText>
-                            </CardBody>
-                        </Card>
-                        <br />
+                        <Row className={"assPad"}>
+                            <Col>
+                                <NavLink href="#">
+                                    <Card body key={key} className="text-center">
+                                        <CardTitle className={"assTitle"}>
+                                            {props.homeworks[index].name}
+                                        </CardTitle>
+                                    </Card>
+                                </NavLink>
+                            </Col>
+                        </Row>
+                        <br/>
                     </Col>
                 })}
-            </Row>
         </div>
     )
 };
