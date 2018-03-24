5ebac09a736d0c82833021e86db943dc1a5cee62
Walter Jacquette <wjacquet@purdue.edu>
2018-03-23 21:07:21 -0400

    Making new Branch to upgrade

--- src/CreateActivity/Instruct.js
+++ src/CreateActivity/Instruct.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 
 import { Container, Row, Col, FormGroup, Label, Input} from 'reactstrap';
 
+
+import { Button, Card, CardBody, CardImage, CardTitle, CardText } from 'mdbreact';
 import './Instruct.css';
 
 class Instruct extends Component {
@@ -26,6 +28,14 @@ class Instruct extends Component {
                         <br/>
                         <br/>
                         <br/>
+                        <Card>
+                            <CardImage className="img-fluid" src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20%282%29.jpg" />
+                            <CardBody>
+                                <CardTitle>Card title</CardTitle>
+                                <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+                                <Button href="#">Button</Button>
+                            </CardBody>
+                        </Card>
                     </Col>
                     <Col xs={4} style={{backgroundColor: 'red'}}>
                         <p>Hey there bud</p>
