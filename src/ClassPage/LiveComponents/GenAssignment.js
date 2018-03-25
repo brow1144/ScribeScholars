2f3d33c2d0f5ef8fcdf8d3b3aa5c6754e6c956b5
Jeremy <jeremyputput@gmail.com>
2018-03-25 17:40:10 -0400

    Inclass now sort of can be navigated

--- src/ClassPage/LiveComponents/GenAssignment.js
+++ src/ClassPage/LiveComponents/GenAssignment.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import { Nav, NavLink, Container, Row, Col, FormGroup, Label, Input } from 'reactstrap';
+import { Nav, NavLink, Container, Row, Col, Button, FormGroup, Label, Input } from 'reactstrap';
 import { NavLink as RouterLink } from 'react-router-dom'
 import {firestore} from "../../base";
 import MCQ from "./MCQ";
@@ -30,6 +30,7 @@ class GenAssignment extends Component {
       class: null,
       questions: null,
 
+      numOfQuestions: null,
       currentScore: null,
       currentQuestion: null,
       completed: null,
@@ -49,23 +50,15 @@ class GenAssignment extends Component {
     let self = this;
 
     let quest = this.state.questions;
-    console.log(this.state.currentQuestion - 1);
-    console.log(quest[0].option1);
 
     self.setState({
-      option1: "hello",
-      option2: quest[0].option2,
-      option3: quest[0].option3,
-      option4: quest[0].option4,
-      prompt: quest[0].prompt,
-      type: quest[0].type,
-    }, () => {
-
-      console.log(this.state.option1);
-
+      option1: quest[this.state.currentQuestion-1].option1,
+      option2: quest[this.state.currentQuestion-1].option2,
+      option3: quest[this.state.currentQuestion-1].option3,
+      option4: quest[this.state.currentQuestion-1].option4,
+      prompt: quest[this.state.currentQuestion-1].prompt,
+      type: quest[this.state.currentQuestion-1].type,
     });
-
-    //this.state.question: this.state.assignments[0].questions[index - 1]
 
   };
 
@@ -108,6 +101,7 @@ class GenAssignment extends Component {
           currentQuestion: doc.data().currentQuestion,
           completed: doc.data().completed,
           answerHistory: doc.data().answerHistory,
+          numOfQuestions: doc.data().numOfQuestions,
         }, () => {
           self.setQuestion();
         })
@@ -117,58 +111,48 @@ class GenAssignment extends Component {
     });
   };
 
-  grabQuestion = (index) => {
+  decPage = () => {
     let self = this;
 
-    // the classes' assignment collection reference
-    let homeworkRef = firestore.collection("classes").doc(this.props.code).collection("inClass").doc();
-    homeworkRef.get().then(function(doc) {
+    let user = firestore.collection("users").doc(this.state.uid).collection("inClass").doc(this.state.lessonNumber)
+
+    user.get().then((doc) => {
       if (doc.exists) {
-        if (doc.data().questions != null) {
-          self.setState({
-            question: doc.data().questions(index),
-          });
+        if(doc.data().currentQuestion-1 > 0) {
+          //console.log("Pre dec: " + this.state.currentQuestion);
+          user.update({
+            currentQuestion: this.state.currentQuestion - 1,
+          }, () => {
+            self.getAssignments(this.props.class)
+          })
+
+          //console.log("Post dec: " + doc.data().currentQuestion);
         }
-      } else {
-        console.log("user not found");
       }
-
-
-    }).catch(function(error) {
-      console.log("Error getting document: ", error);
-    });
-  };
-
-  updateAssignment = () => {
-    /*
-    let studentRef = firestore.collection("users").doc(self.state.uid);
-    studentRef.get().then(function(doc) {
-      if (doc.exists) {
-        let data = doc.data();
-        tclasses = data.classes;
-
-        for (let j in tclasses) {
-          if (tclasses[j].code === code)
-            tclasses[j].class = newName;
-        }
-
-        teacherRef.update({
-          classes: tclasses,
-        })
-          .then(function() {
-            console.log("Document successfully updated!");
-          }).catch(function(error) {
-          // The document probably doesn't exist.
-          console.error("Error updating document: ", error);
-        });
-        self.props.updateClasses(tclasses);
-      } else {
-        console.log("No such document!");
-      }
-    }).catch(function(error) {
+    }).catch((error) => {
       console.log("Error getting document:", error);
     });
-*/
+  }
+
+  incPage = () => {
+    let self = this;
+
+    let user = firestore.collection("users").doc(this.state.uid).collection("inClass").doc(this.state.lessonNumber)
+
+    user.get().then((doc) => {
+      if (doc.exists) {
+        if(doc.data().currentQuestion+1 <= this.state.numOfQuestions) {
+          //console.log("Pre inc: " + this.state.currentQuestion);
+          user.update({
+            currentQuestion: this.state.currentQuestion + 1,
+          }, () => {
+            self.getAssignments(this.props.class)
+          })
+        }
+      }
+    }).catch((error) => {
+      console.log("Error getting document:", error);
+    });
   }
 
   render() {
@@ -176,8 +160,17 @@ class GenAssignment extends Component {
       <div className={"center"}>
         <Container fluid >
           <Row>
-            <Col>
-              <MCQ/>
+            <Col xs={12}>
+              <MCQ currentQuestion = {this.state.currentQuestion} name = {this.state.name} prompt = {this.state.prompt} option1 = {this.state.option1}
+                   option2 ={this.state.option2} option3 = {this.state.option3} option4 = {this.state.option4}/>
+            </Col>
+          </Row>
+          <Row>
+            <Col xs={6}>
+              <Button onClick={this.decPage}>Last Question</Button>
+            </Col>
+            <Col xs={6}>
+              <Button onClick={this.incPage}>Next Question</Button>
             </Col>
           </Row>
         </Container>
--- src/ClassPage/LiveComponents/MCQ.js
+++ src/ClassPage/LiveComponents/MCQ.js
@@ -4,35 +4,37 @@ import { Nav, NavLink, Container, Row, Col, FormGroup, Label, Input } from 'reac
 import { NavLink as RouterLink } from 'react-router-dom'
 import {firestore} from "../../base";
 
+import "./MCQ.css"
 
 const MCQ = (props) => {
 
   return (
     <FormGroup tag={"fieldset"}>
-      <legend className={"RadioTitle"}>#1 This Is The Question Header?</legend>
+      <legend className={"RadioTitle"}>{props.name}: Question {props.currentQuestion}</legend>
+      <legend className={"RadioTitle"}>{props.prompt}</legend>
       <Col sm={{size: 10}}>
         <FormGroup>
           <Label className={"RadioLabel"}>
             <Input className={"RadioButton"} type="radio" name="radio1"/>{' '}
-            <p>`oh hey bud ${props.question}`</p>
+            <p className={"textSize"}>{props.option1}</p>
           </Label>
         </FormGroup>
         <FormGroup>
           <Label className={"RadioLabel"}>
             <Input className={"RadioButton"} type="radio" name="radio1"/>{' '}
-            <p>Possible Answer Number 2</p>
+            <p className={"textSize"}>{props.option2}</p>
+          </Label>
+        </FormGroup>
+        <FormGroup>
+          <Label className={"RadioLabel"}>
+            <Input className={"RadioButton textSize"} type="radio" name="radio1"/>
+            <p className={"textSize"}>{props.option3}</p>
           </Label>
         </FormGroup>
         <FormGroup>
           <Label className={"RadioLabel"}>
             <Input className={"RadioButton"} type="radio" name="radio1"/>
-            <p>Possible Answer Number 3</p>
-          </Label>
-        </FormGroup>
-        <FormGroup>
-          <Label className={"RadioLabel"}>
-            <Input className={"RadioButton"} type="radio" name="radio1"/>
-            <p>Possible Answer Number 3</p>
+            <p className={"textSize"}>{props.option4}</p>
           </Label>
         </FormGroup>
       </Col>
