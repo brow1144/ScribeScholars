68d7710ebafabdc9a335fb7193d0f83fe0d89d43
Eric Thiem <ericthiem23@gmail.com>
2018-03-25 16:46:26 -0700

    Formatted and added total point count

--- src/ClassPage/GradesTable.js
+++ src/ClassPage/GradesTable.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import {Table, Container, Row, Col, Label } from 'reactstrap';
 import { firestore } from "../base";
+import { Modal } from 'react-modal';
 import { NavLink as RouterLink } from 'react-router-dom'
 import './Table.css'
 
@@ -12,6 +13,8 @@ class GradesTable extends Component {
     this.state = {
       uid: this.props.uid,
       code: this.props.code,
+      score: 0,
+      max_score: 0,
       gradeData: [],
       doneLoading: false
     };
@@ -24,12 +27,30 @@ class GradesTable extends Component {
 
     }).then( function () {
 
+      for(let i = 0; i < self.state.gradeData.length; i++){
+        self.addScore(self.state.gradeData[i].score, self.state.gradeData[i].maxscore);
+      }
+
       self.setState({ doneLoading: true });
 
     }).catch(function (error) {
       console.log("Error getting document:", error);
     });
   }
+
+  addScore = (score, maxscore) => {
+    let self = this;
+    let sc = +self.state.score;
+    let m_sc = +self.state.max_score;
+
+    sc += +score;
+    m_sc += +maxscore;
+
+    self.setState({
+      score: sc,
+      max_score: m_sc,
+    });
+  };
 
   render() {
 
@@ -69,7 +90,7 @@ class GradesTable extends Component {
                     <th>Assignment</th>
                     <th>Score</th>
                     <th>Max Score</th>
-                    <th>Request Regrade</th>
+                    <th>Regrades</th>
                   </tr>
                   </thead>
                   <tbody>
@@ -80,7 +101,7 @@ class GradesTable extends Component {
                       <td>{this.state.gradeData[index].maxscore}</td>
                       <td>
                         <RouterLink to={`practiceQuestion`}>
-                          Link
+                          Request Regrade
                         </RouterLink>
                       </td>
                     </tr>
@@ -94,6 +115,20 @@ class GradesTable extends Component {
               <Col className={"moreSpace"}>
               </Col>
             </Row>
+
+            <Row>
+              <Col xs={12}>
+                <Table>
+                  <thead>
+                  <tr>
+                    <th>Total: {this.state.score}/{this.state.max_score}</th>
+                  </tr>
+                  </thead>
+                </Table>
+              </Col>
+            </Row>
+
+
             <Row>
               <Col className={"moreSpace"}>
               </Col>
