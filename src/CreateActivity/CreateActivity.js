52c171ec137f6ddeb471643d7a5b2e079e297a69
Walter Jacquette <wjacquet@purdue.edu>
2018-03-27 15:08:55 -0400

    Prevent other question types in lesson

--- src/CreateActivity/CreateActivity.js
+++ src/CreateActivity/CreateActivity.js
@@ -277,7 +277,7 @@ class CreateActivity extends Component {
                                 self.setNewDoc();
                             } else {
                                 studentRef.set({
-                                    answer: tempHist,
+                                    answers: tempHist,
                                     class: self.props.class,
                                     completed: 0,
                                     currentQuestion: 1,
@@ -298,31 +298,31 @@ class CreateActivity extends Component {
                             console.log("Error getting document: ", error);
                         });
                     }
-/*                    studentRef.get().then(function (doc) {
-                        if (doc.exists) {
-                            self.setNewDoc();
-                        } else {
-                            studentRef.set({
-                                answerHistory: tempAnsHis,
-                                class: self.props.class,
-                                completed: "",
-                                currentQuestion: 1,
-                                currentScore: 0,
-                                maxscore: self.state.questions.length,
-                                name: self.state.title,
-                                numOfQuestions: self.state.questions.length,
-                                questions: tempQuests,
-                                history: tempHist,
+                    /*                    studentRef.get().then(function (doc) {
+                                            if (doc.exists) {
+                                                self.setNewDoc();
+                                            } else {
+                                                studentRef.set({
+                                                    answerHistory: tempAnsHis,
+                                                    class: self.props.class,
+                                                    completed: "",
+                                                    currentQuestion: 1,
+                                                    currentScore: 0,
+                                                    maxscore: self.state.questions.length,
+                                                    name: self.state.title,
+                                                    numOfQuestions: self.state.questions.length,
+                                                    questions: tempQuests,
+                                                    history: tempHist,
 
-                            }).then(function () {
-                                console.log("successfully written!");
-                            }).catch(function (error) {
-                                console.log(error);
-                            });
-                        }
-                    }).catch(function (error) {
-                        console.log("Error getting document: ", error);
-                    });*/
+                                                }).then(function () {
+                                                    console.log("successfully written!");
+                                                }).catch(function (error) {
+                                                    console.log(error);
+                                                });
+                                            }
+                                        }).catch(function (error) {
+                                            console.log("Error getting document: ", error);
+                                        });*/
                 }
             }
         })
@@ -377,13 +377,16 @@ class CreateActivity extends Component {
                                                             ?
                                                             <MCQForm question={quest} index={index}
                                                                      recordQuestion={this.recordQuestion}/>
-                                                            : (quest.type === "FRQ")
+                                                            : (quest.type === "FRQ" && this.props.assType !== "Lesson")
                                                                 ?
                                                                 <FRQForm question={quest} index={index}
                                                                          recordQuestion={this.recordQuestion}/>
-                                                                :
-                                                                <VideoForm question={quest} index={index}
-                                                                           recordQuestion={this.recordQuestion}/>
+                                                                : (quest.type === "VIDEO" && this.props.assType !== "Lesson")
+                                                                    ?
+                                                                    <VideoForm question={quest} index={index}
+                                                                               recordQuestion={this.recordQuestion}/>
+                                                                    :
+                                                                    <div/>
                                                         }
                                                     </AccordionItemBody>
                                                 </AccordionItem>
