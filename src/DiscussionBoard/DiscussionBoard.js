4b1612967cbe7ad2db8ab12ad3b33d05f882a947
Kyle Brown <brow1144@purdue.edu>
2018-04-04 11:56:32 -0400

    Doable repsonsivness

--- src/DiscussionBoard/DiscussionBoard.js
+++ src/DiscussionBoard/DiscussionBoard.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 
-import { Col, Row, InputGroup, InputGroupAddon, Input, Button} from 'reactstrap';
+import { Col, Row, InputGroup, InputGroupAddon, Input} from 'reactstrap';
+
+import DiscussionQuestion from '../DiscussionBoard/DiscussionBoard';
 
 import '../DiscussionBoard/DiscussionBoard.css'
 
@@ -54,67 +56,7 @@ class DiscussionBoard extends Component {
         <br/>
         <br/>
 
-        <Row>
-          <Col sm='0' md='2'/>
-          <Col sm='12' md='8'>
-            <Row>
-              <Col className='center' md='2'>
-                <h4 className='recent'>Recent</h4>
-              </Col>
-              <Col className='center' md='2'>
-                <h4 className='popular'>Popular</h4>
-              </Col>
-              <Col className='center' md='2'>
-                <h4 className='lastReply'>Last Reply</h4>
-              </Col>
-              <Col md='4'/>
-              <Col md='2'>
-                <Button className='exSpace' color='success'>+ New Thread</Button>
-              </Col>
-            </Row>
-            <Row className='questionBox'>
-              <Col xs='4' md='1'>
-                <img className="userImage"
-                     src='https://yt3.ggpht.com/a-/AJLlDp2XDF1qkCbGAr7HiDi6ywCWp3JfwN3vgN6ksA=s900-mo-c-c0xffffffff-rj-k-no'
-                     alt="userIcon" />
-              </Col>
-              <Col xs='8' md='4'>
-                <h3 className='questionText'>
-                  How to start investing
-                </h3>
-                <p className="searchSubTitle">
-                  by Kyle Brown
-                </p>
-              </Col>
-              <Col xs='0' md='2'/>
-              <Col xs='11' md='3'>
-                <h4 className='hashtag'>
-                  # Investing
-                </h4>
-              </Col>
-              <Col xs='10' md='1'>
-                <Row>
-                  <p className='response'>
-                    Teacher:
-                  </p>
-                  <i className="fas fa-check check"/>
-                </Row>
-                <Row>
-                  <p className='response'>
-                    Student:
-                  </p>
-                  <i className="fas fa-times times"/>
-                </Row>
-              </Col>
-              <Col xs='11' md='1'>
-                  <h4 className='replyNum'>78</h4>
-                  <p className='replies'>Views</p>
-              </Col>
-            </Row>
-          </Col>
-          <Col sm='0' md='2'/>
-        </Row>
-
+        <DiscussionQuestion />
 
         <br/>
         <br/>
