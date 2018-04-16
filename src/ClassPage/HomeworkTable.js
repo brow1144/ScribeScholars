ea35c2a522f6704897102e8a248a3940c47603f1
Walter Jacquette <wjacquet@purdue.edu>
2018-04-16 19:26:15 -0400

    Make the Edit Page work for Homeworks

--- src/ClassPage/HomeworkTable.js
+++ src/ClassPage/HomeworkTable.js
@@ -287,6 +287,12 @@ class HomeworkTable extends Component {
                                     :
                                     <th/>
                                   }
+                                  {this.state.role === "teacher"
+                                    ?
+                                    <th>Edit Link</th>
+                                    :
+                                    <th/>
+                                  }
                                 </tr>
                                 </thead>
                               {Object.keys(this.state.homeworks).map((key, index) => {
@@ -331,6 +337,17 @@ class HomeworkTable extends Component {
                                         ?
                                         <td>
                                           <Button onClick={() => this.openModal(this.state.homeworks[index].lessonCode)}>View</Button>
+                                        </td>
+                                        :
+                                        <td/>
+                                      }
+                                      {this.state.role === "teacher"
+                                        ?
+                                        <td>
+                                          <RouterLink
+                                            to={`/ScribeScholars/HomePage/${this.props.code}/homework/edit-activity/${this.state.homeworks[index].lessonCode}`}>
+                                            Edit
+                                          </RouterLink>
                                         </td>
                                         :
                                         <td/>
